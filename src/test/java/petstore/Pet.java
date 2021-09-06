96d2536f1bbc0447691fdd10b57bfa61baab42a2
rrosinha <rosinha1982@hotmail.com>
2021-09-06 08:46:15 +0100

    Completed the deletePet (DELETE) script

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -86,4 +86,21 @@ public class Pet {
         ;
     }
 
+    @Test(priority = 4)
+    public void deletePet() {
+        String petId = "1904090598";
+
+        given()
+                .contentType("application/json")
+                .log().all()
+        .when()
+                .delete(uri + "/" + petId)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("code", is(200))
+                .body("type", is("unknown"))
+                .body("message", is(petId))
+        ;
+    }
 }
